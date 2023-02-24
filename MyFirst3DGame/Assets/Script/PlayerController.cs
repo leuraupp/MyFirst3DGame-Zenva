2f1a4801ce2d47903b39d9bde975f8415b1f65f5
leuraupp <leuraupp@gmail.com>
2023-02-24 00:00:54 -0300

    Lesson 24/02/2023

--- MyFirst3DGame/Assets/Script/PlayerController.cs
+++ MyFirst3DGame/Assets/Script/PlayerController.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody rig;
+
+    private bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float z = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+        Vector3 vel = rig.velocity;
+        vel.y = 0;
+
+        if (vel.x != 0 || vel.z != 0) {
+            transform.forward = vel;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            isGrounded = false;
+            rig.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if (transform.position.y < -5) {
+            GameOver();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
         
+        if (collision.GetContact(0).normal == Vector3.up) {
+            isGrounded = true;
+        }
+
+    }
+
+    public void GameOver() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
